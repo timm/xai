f102e771ec9a60ab89b894f519513b01de8af695
timm <timm@ieee.org>
2026-01-17 14:02:37 -0500

    sas

--- tmp.py
+++ tmp.py
@@ -5,7 +5,9 @@ six.py: stochastic incremental multi-objective expalanation
 
 Options:
    -b bins=7   Number of bins
+   -l leaf=2   Min examples in leaf of tree
    -s seed=1   Random number seed
+
 """
 import re,ast,sys,random
 from math import log,exp,sqrt
@@ -14,10 +16,10 @@ BIG=1e32
 #------------------------------------------------------------------------------
 # create
 def NUM():       return obj(it=NUM,  n=0, mu=0, m2=0, sd=0)
-def ROW(lst):    return obj(it=ROW,  raw=lst, bins=lst, y=0)
+def ROW(lst):    return obj(it=ROW,  raw=lst, bins=lst[:], y=0)
 def DATA(src): 
   src  = iter(src)
-  data = obj(it=DATA, rows=[], cols=COLS(next(src)), nums={}, tally={})
+  data = obj(it=DATA, rows=[], cols=COLS(next(src)), tally={})
   return Data(data,src)
 
 def COLS(names):
@@ -34,7 +36,7 @@ def Data(data, lsts):
   for lst in lsts:
     for c,num in data.cols.nums.items(): Num(num, lst[c])
     data.rows += [ROW(lst)]
-  data.n = {}
+  data.tally = {}
   for row in data.rows: Row(data, row)
   return data
 
@@ -49,35 +51,42 @@ def Num(num,v):
 def Row(data, row):
   for c,num in data.cols.nums.items():
     if (v := row.raw[c]) != "?":
-      row.bins[c] = v2b(v, num.mu, num.sd)
-  row.y = disty(data,row)
+      row.bins[c] = int(the.bins * norm(num,v))
+  row.y = disty(data, row)
   for c in data.cols.x:
-    if (v := row.raw[c]) != "?":
-      data.tally[(c,v)] = data.tally.get((c,v),0) + row.y
+    if (v := row.bins[c]) != "?":
+      k = (c,v)
+      if k not in data.tally: data.tally[k] = NUM()
+      Num(data.tally[k], row.y)
 
-#------------------------------------------------------------------------------
-def distx(data, row1, row2):
-  def fn(a,b):
-    if a==b=="?": return 1
-    mid = (the.bins - 1) / 2
-    if a=="?": a = (0 if b > mid else the.bins-1)
-    if b=="?": b = (0 if a > mid else the.bins -1)
-    return abs(a - b) / (the.bins - 1)
-  xs = data.cols.x
-  return sqrt(sum(fn(row1.bins[c], row2.bins[c])**2 for c in xs) / len(xs))
+def score(num): 
+  return BIG if num.n < the.leaf else num.mu + num.sd /(sqrt(num.n) + 1/BIG)
 
 def disty(data, row):
-  ys = data.cols.y.items()
-  return sqrt(sum(abs(row.bins[c]/(the.bins-1) - goal) for c,goal in ys) / len(ys))
+  raw, nums, ys = row.raw, data.cols.nums, data.cols.y.items()
+  return sqrt(sum(abs(norm(nums[c],raw[c]) - goal) for c,goal in ys) / len(ys))
+
+def norm(num,v): # converts v to an integer 0..the.bins-1
+  z = max(-3, min(3, (v-num.mu) / (num.sd+1/BIG)))
+  return 1 + exp(-1.7 * z)
+
+def b2v(b,mu,sd): # converts b to a real number (lower bound on each bin)
+  eps = 1/BIG
+  p = min(1-eps, max(eps, b/the.bins))
+  return mu + max(-3, min(3, log(p/(1-p))/1.7)) * (sd + eps)
 
 #-------------------------------------------------------------------------------
+def o(v):
+  if isinstance(v, float): return round(v, 2)
+  if isinstance(v, (list, tuple, set)): return [o(x) for x in v]
+  if isinstance(v, dict):
+    return "[" + " ".join([f":{k} {o(val)}" for k, val in v.items()]) + "]"
+  if hasattr(v, '__name__'): return v.__name__
+  if hasattr(v, '__dict__'): return f"{type(v).__name__}{o(vars(v))}"
+  return v
+
 class obj(dict):
-  __getattr__ = dict.__getitem__; __setattr__ = dict.__setitem__
-  def __repr__(i):
-    def say(k,v): 
-      if isinstance(v,float): return round(v,2)
-      return v.__name__ if type(v)==type(disty) else v
-    return "{" + " ".join([f":{k} {say(k, i[k])}" for k in i]) + "}"
+  __getattr__, __setattr__, __repr__ = dict.__getitem__, dict.__setitem__, o
 
 def era(src, size=20):
   cache = []
@@ -88,18 +97,6 @@ def era(src, size=20):
 
 def shuffle(lst): random.shuffle(lst); return lst
 
-def v2b(v,mu,sd,eps=1/BIG):
-  z = max(-3, min(3, (v-mu) / (sd+eps)))
-  return int(the.bins / (1 + exp(-1.7 * z)))
-
-def b2v(b,mu,sd,eps=1/BIG):
-  p = min(1-eps, max(eps, b/the.bins))
-  return mu + max(-3, min(3, log(p/(1-p))/1.7)) * (sd + 1/BIG)
-
-#------------------------------------------------------------------------------
-def config(s=__doc__):
-  return obj(**{m[0]:coerce(m[1]) for m in re.findall(r"(\w+)=(\S+)", s)})
-
 def coerce(s):
   try: return ast.literal_eval(s)
   except (ValueError, SyntaxError): return s.strip()
@@ -109,30 +106,28 @@ def csv(fileName):
     for l in f:
       if l: yield [coerce(x) for x in l.split(",")]
 
+def config(s=__doc__):
+  return obj(**{m[0]:coerce(m[1]) for m in re.findall(r"(\w+)=(\S+)", s)})
+
 def cli(funs,d,flags):
   for n, s in enumerate(flags):
     v = coerce(flags[n + 1]) if n < len(flags) - 1 else None
-    if f := funs.get(f"go{s.replace('-', '_')}"):
-      f(v) if v is not None else f()
-    else:
-      k = s.lstrip("-")[0]
-      if k in d: d[k] = v
+    if f := funs.get(f"go{s.replace('-', '_')}"): f(v)
+    elif (k := s.lstrip("-")[0]) in d: d[k] = v
 
-def go_h()     : print(__doc__)
-def go__the()  : print(the)
+def go_h(_)    : print(__doc__)
+def go__the(_) : print(the)
 def go_s(n)    : the.seed=n; random.seed(n)
-def go__bins(_):
-  for n in range(90,110):
-     b=v2b(n,100,5)
-     v=b2v(b,100,5)
-     print(n,b,v)
-
 def go__csv(f) : [print(row) for row in csv(f)]
-def go__data(f): 
-    tally= min(DATA(csv(f)).tally.items(), key=lambda x: x[1])
-    print(tally)
+def go__ys(f): 
+    data = DATA(csv(f))
+    print(*data.cols.names)
+    [print(row) for row in sorted(data.rows, key=lambda r: disty(data,r))[::40]]
+
+def go__tally(f): 
+    print(o(min(DATA(csv(f)).tally.items(), key=lambda x: score(x[1]))))
 
 #------------------------------------------------------------------------------
-the=config()
+the = config()
 random.seed(the.seed)
 if __name__=="__main__": cli(vars(),the,sys.argv)
